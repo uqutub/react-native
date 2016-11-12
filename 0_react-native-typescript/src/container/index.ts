5204dc5fa01b7be38848e6b94a109cefe882e269
dev <developer@dev-pc.local>
2016-11-12 19:34:50 +0500

    react-native-typescript-hello-world

---
+++ 0_react-native-typescript/src/container/index.ts
@@ -0,0 +1,12 @@
+import MainContainerAndroid from './main/main.android';
+import MainContainerIOS from './main/main.android';
+
+export let AppContainers = [
+    MainContainerAndroid,
+    MainContainerIOS
+]
+
+export {
+    MainContainerAndroid,
+    MainContainerIOS
+}
